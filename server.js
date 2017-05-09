30ea884ca13ef163f4d854b896304c89338b346c
Joakim Ljungren <joakim@ljungren.se>
2017-05-09 15:18:44 +0200

    deploy shell

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express  = require('express'),
   bodyParser = require('body-parser'),
-  service = express();
-  // config = require('./config.js');
+  service = express(),
+  config = require('./config.js')
+
 
 service.use(bodyParser.urlencoded({ extended: true }));
 service.use(bodyParser.json());
@@ -14,40 +15,52 @@ service.use((req, res, next) => {
 });
 
 service.get('/', (req,res,next) => {
-  return res.status(200).send('Welcome, api available at /search');
+  return res.status(200).send('api available at /search');
 });
 
 
 service.post('/search', (req,res,next) => {
 
-    // exBody = {
-    //   company_profile: ''
-    // };
+  //example data
+  let result = {
+    "source": "agent",
+    "resolvedQuery": "we are a design agency",
+    "action": "maps.search",
+    "actionIncomplete": false,
+    "parameters": {
+      "company-domain": "design",
+      "company-type": "agency"
+    }
+  }
+  console.log('example data: ' + JSON.stringify(result.parameters));
+  console.log('request data: ' + req.body.result.parameters);
 
-    // get data
-    // let data = req.body;
-    // //location and company profile
+  // get data
+  // let data = req.body;
+  // //location and company profile
 
-    // request('https://maps.googleapis.com/maps/api/place/nearbysearch/output?'+"", (error, response, body) => {
-    //     if (!error && response.statusCode == 200) {
-    //         if(body==null || body == undefined || body == false){
-    //             res.status(422).json([{"msg":"Nothing found","value":""}]);
-    //             return;
-    //         }
-    //         else{
-    //             return res.status(200).send(body);
-    //         }
-    //     }
-    // });
+  // request('https://maps.googleapis.com/maps/api/place/nearbysearch/output?'+"", (error, response, body) => {
+  //     if (!error && response.statusCode == 200) {
+  //         if(body==null || body == undefined || body == false){
+  //             res.status(422).json([{"msg":"Nothing found","value":""}]);
+  //             return;
+  //         }
+  //         else{
+  //             return res.status(200).send(body);
+  //         }
+  //     }
+  // });
 
-    console.log('req params: '+ JSON.stringify(req.body.result.parameters));
-    // console.log('response: '+ JSON.stringify(res));
-    var test = req.body.result && req.body.result.parameters && req.body.result.parameters.location ? req.body.result.parameters.location : "This is sample response"
-    return res.json({
-      speech: test,
-      displayText: test,
-      source: 'mio-service'
-    });
+  //console.log('req params: '+ JSON.stringify(req.body.result.parameters));
+  // console.log('response: '+ JSON.stringify(res));
+  var test = req.body.result && req.body.result.parameters && req.body.result.parameters.location ? req.body.result.parameters.location : "This is sample response"
+  return res.json({
+    speech: 'This is a sample response',
+    displayText: 'This is a sample response',
+    source: 'mio-service',
+    contextOut: [{"name":"office", "lifespan":2, "parameters":{"city":"Rome"}}],
+
+  });
 
 });
 
