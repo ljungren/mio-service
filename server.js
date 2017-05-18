07b1ee790c22b8f3c8036afb838d5a1cc261f336
Joakim Ljungren <joakim@ljungren.se>
2017-05-18 16:17:18 +0200

    misc

--- server.js
+++ server.js
@@ -3,59 +3,49 @@
 const express  = require('express'),
   bodyParser = require('body-parser'),
   service = express(),
+  request = require('request'),
   config = require('./config.js'),
-  attachments = require('./attachments.json');
+  attachments = require('./attachments.json')
 
 
-service.use(bodyParser.urlencoded({ extended: true }));
-service.use(bodyParser.json());
+service.use(bodyParser.urlencoded({ extended: true }))
+service.use(bodyParser.json())
 
 service.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
+  console.log(req.method, req.url)
+  next()
+})
 
 service.get('/', (req,res,next) => {
-  return res.status(200).send('api available at /search');
-});
+  return res.status(200).send('api available at /search and /info')
+})
 
 
 service.post('/search', (req,res,next) => {
 
-  //example data
-  let result = {
-    "source": "agent",
-    "resolvedQuery": "we are a design agency",
-    "action": "maps.search",
-    "actionIncomplete": false,
-    "parameters": {
-      "company-domain": "design",
-      "company-type": "agency"
-    }
+  //pass parameters? delayed slack response possible
+
+  let temp = {
+    speech: "Thanks for the info! I'll get back to you with what I find in a sec.",
+    displayText: "Thanks for the info! I'll get back to you with what I find in a sec.",
+    source: "mio-service",
   }
-  //console.log('example data: ' + result.parameters);
-  //console.log('request data: ' + req.body.result.parameters);
+  //res.write(JSON.stringify(temp))
+  //res.write(attachments)
+  // return res.end()
+  
+  return res.status(200).send(attachments)
 
-  // setTimeout(() => {
-    return res.json({
-      speech: 'This is a sample response',
-      displayText: 'This is a sample response',
-      source: 'mio-service',
-    });
-    // return res.status(200).send(attachments);
-  // }, 5000);
-  // console.log('response: '+attachments);
-  // return res.status(200).send(attachments);
-
-});
+})
 
 service.post('/info', (req,res,next) => {
   //respond with info about requested location/office (sliperiet, house be?)
-
-});
+  res.send('poop')
+})
 //start Server
 const server = service.listen((process.env.PORT || 9000), () => {
 
-   console.log("Listening to port %s",server.address().port);
+   console.log("Listening to port %s",server.address().port)
 
-});
+})
+
