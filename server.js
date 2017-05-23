ffb192fd140e47275e4a1f7750feae15f8379860
Joakim Ljungren <joakim@ljungren.se>
2017-05-23 13:33:28 +0200

    fix response formatting

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ service.post('/interaction', (req,res,next) => {
 
   let action = data.actions[0].value
   let context = data.callback_id
-  console.log(context + ': ' + action);
+  console.log(context + ': ' + action)
 
   switch(action){
     case 'contact':
@@ -98,7 +98,7 @@ let moreInfo = (context) => {
       res.text = '*I found 8 companies and 33 persons in this region that could be of interest to your company profile. Description:*\nThe Great Northern is a tech/cultural/creative melting pot in Skellefteå where design, technology and business blend to form a thriving startup community. It is an ambitious mix of co-working space, incubators, event arenas, startups and established businesses. We offer office space, fixed seats and flex seats.'
       break
     case 'house_action':
-      res.text = '*I found 4 companies and 26 persons in this region that could be of interest to your company profile. Description:*\n Here you find conference rooms, work areas and social spaces suitable for both lectures, a relaxing sauna and the odd foosball tournament. House Be is the spot where like-minded entrepreneurs, creatives, developers and angel investors meet up, work and socialise. Built around a membership-model. House Be have an offering for both residential companies as well as businesses seeking a temporary solution.'
+      res.text = '*I found 4 companies and 26 persons in this region that could be of interest to your company profile. Description:*\nAt House Be you can find conference rooms, work areas and social spaces suitable for both lectures, a relaxing sauna and the odd foosball tournament. House Be is the spot where like-minded entrepreneurs, creatives, developers and angel investors meet up, work and socialise. Built around a membership-model. House Be have an offering for both residential companies as well as businesses seeking a temporary solution.'
       break
     case 'lounge_action':
       res.text = '*I found 57 companies and 1254 persons in this region that could be of interest to your company profile. Description:*\nBusiness Lounge offer office facilities in Nacka for our network of small businesses, freelancers and branches, etc. In Nacka Strand we offer about 60 offices in landscape environments and twelve offices. In addition, there are plenty of meeting and conference rooms in different sizes. Our premises are bright and fresh, with an open, interactive and energetic office environment that will be inspiring and stimulating to most. We offer a place where you can grow and exchange experiences, etc. with others. Flexible and simple, dynamic and reliable. It is precisely the interactivity between the companies and individuals, where we have a high collective sense of togetherness and sense of shared drive, which makes a big difference to office space.'
