4cdc97388852e7143ef26e63aba6cde01b1ab47a
Joakim Ljungren <joakim@ljungren.se>
2017-06-01 11:58:07 +0200

    test

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   gmaps_places_key: 'AIzaSyAiXYQPwcpnLAeKXtsBE976kfR-JTNO14k',
   slack_token: 'xoxp-180133360992-180728555107-180927255637-621845ac9b3029a7b5e480ad0ccf622a',
-  slack_bot_token: 'xoxb-181633328150-eixbKpdHyRNRaEtL8uogjR7T'
+  slack_bot_token: 'xoxb-181633328150-eixbKpdHyRNRaEtL8uogjR7T',
+  slack_event_token: 'Km6EFip0qXDXycvmot5WSTJX'
 }
