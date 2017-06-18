543def068516a8907afc7f4ca3fdd63c589d4e06
Joakim Ljungren <joakim@ljungren.se>
2017-06-18 18:58:27 +0200

    remove config

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  gmaps_places_key: 'AIzaSyAiXYQPwcpnLAeKXtsBE976kfR-JTNO14k',
-  slack: {
-    web_token: 'xoxp-180133360992-180728555107-180927255637-621845ac9b3029a7b5e480ad0ccf622a',
-    bot_token: 'xoxb-181633328150-eixbKpdHyRNRaEtL8uogjR7T',
-    event_token: 'Km6EFip0qXDXycvmot5WSTJX'
-  },
-  apiai_access_token: '33e3ab5ee3c546ec9e071305797b4b60'
-}
