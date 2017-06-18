09befdacbbf50d10536d133668b78b31f80a35c6
Joakim Ljungren <joakim@ljungren.se>
2017-06-18 17:50:26 +0200

    edit config

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ module.exports = {
   slack: {
     web_token: 'xoxp-180133360992-180728555107-180927255637-621845ac9b3029a7b5e480ad0ccf622a',
     bot_token: 'xoxb-181633328150-eixbKpdHyRNRaEtL8uogjR7T',
-    event_token: 'Km6EFip0qXDXycvmot5WSTJX'
+    event_token: 'Km6EFip0qXDXycvmot5WSTJX',
+    bot_id: 'U5BJM9N4E'
   },
   apiai_access_token: '33e3ab5ee3c546ec9e071305797b4b60'
 }
