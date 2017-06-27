d0c727e58e70dca04a991525a1c6b0929cf66979
Joakim Ljungren <joakim@ljungren.se>
2017-06-27 17:15:40 +0200

    VERSION 1.0

--- actions/actions.js
+++ actions/actions.js
@@ -218,7 +218,7 @@ let restoreUserSession = (session_id) => {
     // resolve context array
     console.log('session_id restore: '+session_id)
     db.getUser(session_id).then((user)=>{
-      console.log('user contexts: '+JSON.stringify(user.user_session_contexts))
+      // console.log('user contexts: '+JSON.stringify(user.user_session_contexts))
       request({
         url: "https://api.api.ai/v1/contexts?sessionId="+session_id,
         method: "POST",
@@ -235,7 +235,7 @@ let restoreUserSession = (session_id) => {
         }
         else if(body){
           console.log('session post request to api.ai successful!')
-          console.log('api.ai server responded with:', body.names)
+          // console.log('api.ai server responded with:', body.names)
           resolve(body.names)
         }
       })
